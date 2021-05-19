2248e66c8d7ba9429bd05dc98f0bfd76f1b4d08a
Victoria Mihell-Hale <victory_star@hotmail.com>
2021-05-19 19:52:33 +0100

    Add basic validation

--- app.pl
+++ app.pl
@@ -4,10 +4,44 @@ get '/' => sub($c) {
     $c->redirect_to('/form');
 };
 
-get '/form' => 'form';
+get '/form' => sub($c) {
+    $c->render( template => 'form', errors => {} );
+};
 
 post '/form' => sub($c) {
+    my $errors = $c->validate;
+
+    if ( keys %$errors ) {
+        warn "ERRORS";
+        $c->stash( errors => $errors );
+        $c->render( template => 'form' );
+        return;
+    }
+
     $c->redirect_to('/form');
+};
+
+helper validate => sub ($c) {
+    my %errors;
+    my %params = $c->req->body_params->to_hash->%*;
+
+    for my $param_key ( keys %params ) {
+        my $param_value = $params{$param_key};
+
+        warn "$param_key: $param_value";
+
+        $param_value =~ s/^\s+//;
+        $param_value =~ s/\s+$//;
+
+        $errors{"error_empty_$param_key"} = 1 unless $param_value;
+    }
+    use Data::Dumper;
+    $Data::Dumper::Indent   = 2;
+    $Data::Dumper::Maxdepth = 3;
+    $Data::Dumper::Sortkeys = 1;
+    warn Dumper \%errors;
+
+    return \%errors;
 };
 
 app->start;
--- t/app.t
+++ t/app.t
@@ -13,3 +13,7 @@ $t->get_ok('/form');
 $t->post_ok('/form');
 
 done_testing;
+
+# TODO Test utf8
+# test validation of each param
+# test redirect for successful post, re-render for failure
