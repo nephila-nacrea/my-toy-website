72d363d30f8fe60a739bc2c2ec9b6a9011c9aee5
Victoria Mihell-Hale <victory_star@hotmail.com>
2021-05-26 16:33:56 +0100

    Start adding database functionality
    Using SQLite.

---
+++ sql/users.sql
@@ -0,0 +1,6 @@
+CREATE TABLE users (
+    id       INTEGER      PRIMARY KEY AUTOINCREMENT,
+    username VARCHAR(255) NOT NULL UNIQUE,
+    email    VARCHAR(255) NOT NULL UNIQUE,
+    created  INTEGER      NOT NULL
+);
